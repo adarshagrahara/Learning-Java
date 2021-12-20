a7a35b6a4e4c41d06b64da094550ec221ebbb317
boogeyman <Adarsh.agrahara@gmail.com>
2021-12-20 09:49:31 -0800

    interfaces

--- 29. Interfaces/src/main/java/us/adarsh_agrahara/Challenge/Working/Main.java
+++ 29. Interfaces/src/main/java/us/adarsh_agrahara/Challenge/Working/Main.java
@@ -1,22 +1,62 @@
 package us.adarsh_agrahara.Challenge.Working;
 
-import jdk.incubator.vector.VectorOperators;
-
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
-      private static Player e = new Player("hi",2,3);
+     
       
       public static void main(String[] args) {
-            test a = new test("hi",2,3);
-            
-            saveObject(a);
+            Player e = new Player("hi",2,3);
+            System.out.println(e.toString());
+            saveObject(e);
+            e.setHitPoints(8);
+            System.out.println(e.toString());
+            e.setWeapon("Giant Sword");
+            saveObject(e);
+            loadObject(e);
+            System.out.println(e.toString());
       }
+      
+      public static ArrayList<String> readValues() {
+            ArrayList<String> values = new ArrayList<String>();
+            
+            Scanner scanner = new Scanner(System.in);
+            boolean quit = false;
+            int index = 0;
+            System.out.println("Choose\n" +
+                        "1 to enter a string\n" +
+                        "0 to quit");
+            
+            while (!quit) {
+                  System.out.print("Choose an option: ");
+                  int choice = scanner.nextInt();
+                  scanner.nextLine();
+                  switch (choice) {
+                        case 0:
+                              quit = true;
+                              break;
+                        case 1:
+                              System.out.print("Enter a string: ");
+                              String stringInput = scanner.nextLine();
+                              values.add(index, stringInput);
+                              index++;
+                              break;
+                  }
+            }
+            return values;
+      }
+      
       
       public static void saveObject(ISaveable objectToSave) {
             for(int i = 0; i < objectToSave.write().size(); i++){
                   System.out.println("Saving "+objectToSave.write().get(i)+" to storage device");
             }
+      }
+      
+      public static void loadObject(ISaveable objectToLoad) {
+            ArrayList<String> values = readValues();
+            objectToLoad.read(values);
       }
       
       
