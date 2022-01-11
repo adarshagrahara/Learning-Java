dec583c5b94ece6d83e1e118e4625d7feca1bdbe
boogeyman <Adarsh.agrahara@gmail.com>
2022-01-10 22:41:50 -0800

    Gearbox

--- 30. Inner Classes/src/main/java/us/adarsh_agrahara/Gearbox.java
+++ 30. Inner Classes/src/main/java/us/adarsh_agrahara/Gearbox.java
@@ -13,35 +13,40 @@ public class Gearbox {
             this.gears = new ArrayList<Gear>();
             Gear neutral = new Gear(0, 0.0);
             this.gears.add(neutral);
+            
+            for (int i = 0; i < maxGears; i++) {
+                addGear(i,i*5.3);
+            }
       }
       
-      public void operateClutch(boolean in){
+      public void operateClutch(boolean in) {
             this.clutchIsIn = in;
       }
       
       public void addGear(int number, double ratio) {
-            if((number>0) && (number<= this.maxGears)){
+            if ((number > 0) && (number <= this.maxGears)) {
                   this.gears.add(new Gear(number, ratio));
             }
       }
       
-      public void changGear(int newGear){
-            if((newGear >= 0) && (newGear < this.gears.size()) && this.clutchIsIn){
+      public void changGear(int newGear) {
+            if ((newGear >= 0) && (newGear < this.gears.size()) && this.clutchIsIn) {
                   this.currentGear = newGear;
-                  System.out.println("Gear "+newGear+" selected");
-            } else{
+                  System.out.println("Gear " + newGear + " selected");
+            } else {
                   System.out.println("Grind!");
                   this.currentGear = 0;
             }
       }
       
-      public double wheelSpeed(int revs){
-            if(clutchIsIn){
+      public double wheelSpeed(int revs) {
+            if (clutchIsIn) {
                   System.out.println("Scream!!");
                   return 0.0;
             }
             return revs * gears.get(currentGear).getRatio();
       }
+      
       public class Gear {
             private int gearNumber;
             private double ratio;
@@ -52,11 +57,11 @@ public class Gearbox {
                   this.ratio = ratio;
                   
             }
-      
+            
             public double getRatio() {
                   return ratio;
             }
-      
+            
             public double driveSpeed(int revs) {
                   return revs * (this.ratio);
             }
