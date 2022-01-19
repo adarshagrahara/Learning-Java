3b437b281acbcf99d53c759d2e984a727f968399
boogeyman <Adarsh.agrahara@gmail.com>
2022-01-18 22:54:53 -0800

    Added CompareTo interface to compare generic classes

--- 32. Java Generics/src/main/java/us/adarsh_agrahara/GenericsPart2/Team.java
+++ 32. Java Generics/src/main/java/us/adarsh_agrahara/GenericsPart2/Team.java
@@ -2,7 +2,7 @@ package us.adarsh_agrahara.GenericsPart2;
 
 import java.util.ArrayList;
 
-public class Team<T extends Player> {
+public class Team<T extends Player> implements Comparable<Team<T>> {
       int played = 0;
       int won = 0;
       int loss = 0;
@@ -20,40 +20,53 @@ public class Team<T extends Player> {
       
       public boolean addPlayer(T player) {
             if (members.contains(player)) {
-                  System.out.println( player.getName() + " is already on the team");
+                  System.out.println(player.getName() + " is already on the team");
                   return false;
             } else {
                   members.add(player);
-                  System.out.println( player.getName() + " picked for team" + this.name);
+                  System.out.println(player.getName() + " picked for team" + this.name);
                   return true;
             }
             
       }
-      public int numPlayers(){
+      
+      public int numPlayers() {
             return this.members.size();
       }
       
-      public void matchResult(Team<T> opponent, int ourScore, int theirScore){
+      public void matchResult(Team<T> opponent, int ourScore, int theirScore) {
             String message;
-            if(ourScore > theirScore){
+            if (ourScore > theirScore) {
                   this.won++;
                   message = " beat ";
-            } else if(ourScore < theirScore){
+            } else if (ourScore < theirScore) {
                   this.loss++;
                   message = " lost to ";
-            } else{
+            } else {
                   this.tied++;
                   message = " tied with ";
             }
             
             this.played++;
-            if(opponent != null){
-                  System.out.println(this.getName()+message+opponent.getName());
+            if (opponent != null) {
+                  System.out.println(this.getName() + message + opponent.getName());
                   opponent.matchResult(null, theirScore, ourScore);
             }
       }
       
-      public int ranking(){
-            return (won*2) +tied;
+      public int ranking() {
+            return (won * 2) + tied;
+      }
+      
+      @Override
+      public int compareTo(Team<T> team) {
+            if (this.ranking() > team.ranking()) {
+                  return -1;
+            } else if (this.ranking() < team.ranking()) {
+                  return 1;
+            } else {
+                  return 0;
+            }
+            
       }
 }
