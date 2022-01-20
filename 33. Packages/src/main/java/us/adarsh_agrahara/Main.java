0fe2abcf2ddcd89f25a570125632c61e906afaa1
boogeyman <Adarsh.agrahara@gmail.com>
2022-01-19 22:24:17 -0800

    awt packages

---
+++ 33. Packages/src/main/java/us/adarsh_agrahara/Main.java
@@ -0,0 +1,13 @@
+package us.adarsh_agrahara;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		sun.jvm.hotspot.opto.Node node = null;
+		jdk.internal.jimage.ImageReader.Node parent = null;
+		// full name to have both classes from different packages
+	}
+
+}
